ccb43f61502d268b731293e5d8ded2ce9c66722e
ChienHandsome <cerkvena291@gmail.com>
2024-08-29 07:45:49 +0700

    Week02 JS EX1 ArrowFunction

--- EX1/main.js
+++ EX1/main.js
@@ -1,16 +1,21 @@
-let calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-const dolphinsAverage = calcAverage(44, 23, 71);
-const koalasAverage = calcAverage(65, 54, 49);
+const avgDolphins = calcAverage(10, 23, 10);
+const avgKoalas = calcAverage(100, 50, 70);
 
-const checkWinner = (dolphinsAverage, koalasAverage) => {
-  if (dolphinsAverage >= 2 * koalasAverage) {
-    console.log(`Dolphins win (${dolphinsAverage} vs. ${koalasAverage})`);
-  } else if (koalasAverage >= 2 * dolphinsAverage) {
-    console.log(`Koalas win (${koalasAverage} vs. ${dolphinsAverage})`);
-  } 
+const checkWinner = (avgDolhins, avgKoalas) => {
+  if (avgDolhins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolhins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolhins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolhins})`);
+  } else {
+    console.log("No team wins!");
+  }
 };
 
-checkWinner(dolphinsAverage, koalasAverage);
+checkWinner(avgDolphins, avgKoalas);
 
+const avgDolphinsData2 = calcAverage(85, 54, 41);
+const avgKoalasData2 = calcAverage(23, 34, 27);
+checkWinner(avgDolphinsData2, avgKoalasData2);
 
